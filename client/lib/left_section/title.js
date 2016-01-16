cace8529f5b2463edf7f9d7c739218a26e5a6aa2
daz2345 <darrenimpey@btinternet.com>
2016-01-16 20:45:49 +0000

    major refactor

--- client/lib/left_section/title.js
+++
@@ -1,6 +0,0 @@
-Template.title.helpers({
-  pathForHome: function() {
-    var path = FlowRouter.path("home");
-    return path;
-  }
-});
--- client/lib/left_section/user.js
+++
@@ -1,8 +0,0 @@
-Template.user.helpers({
-    userName: function() {
-        return "Darren Impey";
-    },
-    userAccount: function() {
-        return FlowRouter.path("user");
-    }
-})
