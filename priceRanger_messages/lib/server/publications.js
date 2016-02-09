7bc6bcc0f20aa5cbd60fe8c992b47288b1b86c2c
daz2345 <darrenimpey@btinternet.com>
2016-02-09 17:33:36 +0000

    bla

--- priceRanger_messages/lib/server/publications.js
+++ priceRanger_messages/lib/server/publications.js
@@ -1,5 +1,3 @@
 Meteor.publish('liveMessages', function() {
-    if (userIdVal !== null) {
-        return Messages.find({read: false, userId: Meteor.userId()})
-    }
+    return Messages.find({read: false, userId: Meteor.userId()})
 })
