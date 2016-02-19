9db4cc9578b1396a2f9b627cd3558ab176b81581
daz2345 <darrenimpey@btinternet.com>
2016-02-19 13:23:55 +0000

    Remove subs

--- priceRanger_scenarios/lib/scenarios.js
+++ priceRanger_scenarios/lib/scenarios.js
@@ -1,12 +1,5 @@
 Scenarios = new Mongo.Collection("scenarios");
 
-ScenariosSubs = new SubsManager({
-    // maximum number of cache subscriptions
-    cacheLimit: 10,
-    // any subscription will be expire after 5 minute, if it's not subscribed again
-    expireIn: 5
-});
-
 Scenarios.basic = {
       priceStrat_ce:0,
       priceStrat_de:0,
